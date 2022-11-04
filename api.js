7fded6050c213d3e17aada97d1debbea158bab60
abbydemi <116640919+abbydemi@users.noreply.github.com>
2022-11-04 23:26:13 +0100

    added icon to api

---
+++ api.js
@@ -0,0 +1,21 @@
+//using Shecodes api
+
+function displayTemperature(response) {
+  //console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let descriptionElement = document.querySelector("#description");
+
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  descriptionElement.innerHTML = response.data.weather[0].main;
+}
+let apiKey = "fe1afd34e6t43959607dob38ebcb070e";
+let city = "Paris";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
